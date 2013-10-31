d09cd3979187ef51bbb00699fe714bbbe68f0024
dklounge <daviddarden11@gmail.com>
2013-10-31 06:48:29 -0700

    app updated

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,28 @@
 angular.module('myApp', [])
+.provider('Weather', function() {
+  var apiKey = "API_KEY";
+
+  this.getUrl = function(type, ext) {
+    return "http://api.wunderground.com/api/" +
+      this.apiKey + "/" + type + "/q/" +
+      ext + '.json';
+  };
+
+  this.setApiKey = function(key) {
+    if (key) this.apikey = key;
+  };
+
+  this.$get = function($http) {
+    return {
+      // Service object
+    };
+  }
+})
+
+.config(function(WeatherProvider) {
+  WeatherProvider.setApiKey('API_KEY')
+})
+
 .controller('MainCtrl', function($scope, $timeout) {
   // Build the date object
   $scope.date = {};
