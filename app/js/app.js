6537aea2d9c1a250f530b85912142fad352975c2
dklounge <daviddarden11@gmail.com>
2013-11-03 11:04:47 -0800

    added autofill

--- app/js/app.js
+++ app/js/app.js
@@ -29,56 +29,22 @@ angular.module('myApp', ['ngRoute'])
           d.reject(err);
         });
         return d.promise;
+      },
+      getCityDetails: function (query) {
+        var d = $q.defer();
+        $http({
+          method: 'GET',
+          url: "http://autocomplete.wunderground.com/aq?query=" +
+                query
+        }).success(function (data) {
+          d.resolve(data.RESULTS);
+        }).error(function (err) {
+          d.reject(err);
+        });
+        return d.promise;
       }
     };
   };
-})
-
-.config(function (WeatherProvider) {
-  WeatherProvider.setApiKey('5380e57c137f0cef');
-})
-
-.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'templates/home.html',
-      controller: 'MainCtrl'
-    })
-    .when('/settings', {
-      templateUrl: 'templates/settings.html',
-      controller: 'SettingsCtrl'
-    })
-    .otherwise({ redirectTo: '/' });
-})
-
-.controller('MainCtrl', function ($scope, $timeout, Weather, UserService) {
-  // Build the date object
-  $scope.date = {};
-  // Update function
-  var updateTime = function () {
-    $scope.date.raw = new Date();
-    $timeout(updateTime, 1000);
-  };
-
-  // Wunderground weather service
-  $scope.weather = {};
-  $scope.user = UserService.user;
-  Weather.getWeatherForecast($scope.user.location)
-  .then(function (data) {
-    $scope.weather.forecast = data;
-  });
-
-  // Kick off the update function
-  updateTime();
-})
-
-.controller('SettingsCtrl',
-  function ($scope, UserService) {
-    $scope.user = UserService.user;
-
-    $scope.save = function () {
-      UserService.save();
-    }
 })
 
 .factory('UserService', function () {
@@ -102,4 +68,107 @@ angular.module('myApp', ['ngRoute'])
   // Immediatly call restore from the session storage
   service.restore();
   return service;
+})
+
+.config(function (WeatherProvider) {
+  WeatherProvider.setApiKey('5380e57c137f0cef');
+})
+
+.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/home.html',
+      controller: 'MainCtrl'
+    })
+    .when('/settings', {
+      templateUrl: 'templates/settings.html',
+      controller: 'SettingsCtrl'
+    })
+    .otherwise({ redirectTo: '/' });
+})
+
+.directive('autoFill', function ($timeout, Weather) {
+  return {
+    restrict: 'EA',
+    scope: {
+      autoFill: '&',
+      ngModel: '='
+    },
+    compile: function (tEle, tAttrs) {
+      // Our compile function
+      var tplEl = angular.element('<div class="typehead">' +
+        '<input type="text" autcomplete="off" />' +
+        '<ul id="autolist" ng-show="reslist">' +
+          '<li ng-repeat="res in reslist" ' +
+            '>{{ res.name }}</li>' +
+        '</ul>' +
+        '</div>');
+      var input = tplEl.find('input');
+      input.attr('type', tAttrs.type);
+      input.attr('ng-model', tAttrs.ngModel);
+      tEle.replaceWith(tplEl);
+
+      return function (scope, ele, attrs, ctrl) {
+        var minKeyCount = attrs.minKeyCount || 3,
+          timer;
+
+        ele.bind('keyup', function (e) {
+          val = ele.val();
+          if (val.length < minKeyCount) {
+            if (timer) $timeout.cancel(timer);
+            scope.reslist = null;
+            return;
+          } else {
+            if (timer) $timeout.cancel(timer);
+            timer = $timeout(function () {
+              scope.autoFill()(val)
+              .then(function (data) {
+                if (data && data.length > 0) {
+                  scope.reslist = data;
+                  scope.ngModel = data[0].zmw;
+                }
+              });
+            }, 300);
+          }
+        });
+        // Hide the reslist on blur
+        input.bind('blur', function(e) {
+          scope.reslist = null;
+          scope.$digest();
+        });
+      }
+    }
+  }
+})
+
+.controller('MainCtrl', function ($scope, $timeout, Weather, UserService) {
+  // Build the date object
+  $scope.date = {};
+  // Update function
+  var updateTime = function () {
+    $scope.date.raw = new Date();
+    $timeout(updateTime, 1000);
+  };
+
+  // Wunderground weather service
+  $scope.weather = {};
+  $scope.user = UserService.user;
+  Weather.getWeatherForecast($scope.user.location)
+  .then(function (data) {
+    $scope.weather.forecast = data;
+  });
+  // Kick off the update function
+  updateTime();
+})
+
+.controller('SettingsCtrl',
+  function ($scope, UserService, Weather) {
+    $scope.user = UserService.user;
+
+    $scope.save = function () {
+      UserService.save();
+      $location.path('/');
+    }
+
+    $scope.fetchCities = Weather.getCityDetails;
 })
